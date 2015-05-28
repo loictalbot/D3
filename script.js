26c067ddb768fb72207cce284b1cb3af114d1534
Loïc Talbot <loic@loics-macbook-pro.home>
2015-05-28 21:36:34 +0200

    hovering html anchors is linked to d3 nodes hovering

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ path = d3.geo.path().projection(xy);
  
 vis = d3.select("#vis").append("svg:svg").attr("width", 760).attr("height", 960);
 
+function propagateLinks(name, cssClass) {
+	$('#links').append("<div id='"+ cssClass+ "' class='leftCol'><a class='alink link_"+ cssClass+"' href='#'>"+name+"</a></div>");
+}
+
+function highLightOFF(id) {
+	var hoveredItem = d3.select("#id_"+id).style("opacity", 1);		
+	hoveredItem.style("fill", "#3366CC");
+}
+
+function highLightON(id) {
+	var hoveredItem = d3.select("#id_"+id);
+	hoveredItem.style("fill", "#FF6600");
+}
+
 d3.json("http://localhost/france2.json", function(err,json) {
   if (err) console.log(err);
-  console.log (json.features);
+  //console.log (json.features);
   var fct = vis.append("svg:g")
 	   .attr("class", "tracts")
 	   .selectAll("path")
@@ -17,16 +31,15 @@ d3.json("http://localhost/france2.json", function(err,json) {
 	   .enter()
 	   .append("svg:path");
   
-	   fct.attr("class", function(d) { return "region f" + d.properties.ID_1; });
+	   fct.attr("class", function(d) {propagateLinks(d.properties.NAME_1, d.properties.ID_1); return "region f" + d.properties.ID_1; });
 	   fct.attr("id", function(d) { return "id_" + d.properties.ID_1; });
-	   
 	   fct.on("mouseover", function(d) {
-		var hoveredItem = d3.select("#id_"+d.properties.ID_1);
-		hoveredItem.style("fill", "#FF6600");
+	    d3.select(".link_"+d.properties.ID_1).style("text-decoration", "underline");
+		highLightON(d.properties.ID_1);
 	   });
 	   fct.on("mouseout", function(d) {
-		var hoveredItem = d3.select("#id_"+d.properties.ID_1).style("opacity", 1);		
-		hoveredItem.style("fill", "#3366CC");
+		d3.select(".link_"+d.properties.ID_1).style("text-decoration", "none");
+		highLightOFF(d.properties.ID_1);
 	   });
 	   fct.on("click", function(d) {
 		console.log("you have clicked on "+d.properties.ID_1);
@@ -35,5 +48,19 @@ d3.json("http://localhost/france2.json", function(err,json) {
 	  // fct.attr("fill-opacity", 0.5);
 	   //fct.attr("fill", "#FF6600");
 	   fct.attr("stroke", "#222");
+	   
+	setupLinks();
 	return fct;
 });
+
+function setupLinks() {
+   $('.leftCol').hover(function() {
+	  var thisId = $(this).attr('id');
+	  console.log(thisId);
+	  highLightON(thisId);
+   });
+   $('.leftCol').mouseout(function() {
+	  var thisId = $(this).attr('id');
+	  highLightOFF(thisId);
+   });   
+}
